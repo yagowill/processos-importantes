ad9bb4a6fd5cb636d5250df6fd51faad9a5f15e2
yagowill <yagowill3@gmail.com>
2024-05-20 22:10:38 -0300

    raspar dados e imprimir no terminal

---
+++ find_process.py
@@ -0,0 +1,24 @@
+import time
+from wait_element import wait_element
+from selenium.webdriver.common.by import By
+
+def find_process(browser):
+    processes = open("processos.txt", "r").readlines()
+    print("    Processo     | Data Última Tramitação |   Localização Última Tramitação")
+    print("-"*78)
+    for process in processes:
+        protocol_number = process.strip()
+        
+        protocol_input = wait_element(browser, By.XPATH, '//*[@id="protocolo_form_pesq:protocolo"]')
+        protocol_input.clear()
+        protocol_input.send_keys(protocol_number)
+
+        btnPesquisar = wait_element(browser, By.XPATH, '//*[@id="protocolo_form_pesq:botaoPesquisar"]')
+
+        browser.execute_script("arguments[0].click();", btnPesquisar)
+        time.sleep(3)
+        
+        date_last_processing = wait_element(browser, By.XPATH, '//*[@id="protocolo_form_consulta:ultimaTram:0:j_id331"]').text
+        location_last_processing = wait_element(browser, By.XPATH, '//*[@id="protocolo_form_consulta:ultimaTram:0:j_id325"]').text
+        print(f"    {protocol_number}    |      {date_last_processing}     |    {location_last_processing.split("»")[1].strip()}   ")
+        print("-"*78)
--- main.py
+++ main.py
@@ -1,47 +1,21 @@
-from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from browser import browser
+from find_process import find_process
+from login import login
+from wait_element import wait_element
 
-service = Service(ChromeDriverManager().install())
-browser = webdriver.Chrome(service=service)
+browser = browser()
+
+
 url = "https://www.sistemas.pa.gov.br"
-f = open("login.txt", "r")
-user = f.readline().strip()
-passwd = f.readline().strip()
 
-browser.get(f"{url}/governodigital")
-
-
-login_username = browser.find_element(By.ID, "form_login:login_username")
-login_username.send_keys(user)
-
-login_password = browser.find_element(By.ID, "form_login:login_password")
-login_password.send_keys(passwd)
-
-browser.find_element(By.ID, "form_login:button_login").click()
+login(browser,url)
 
 browser.get(f"{url}/eprotocolo")
 
-element = WebDriverWait(browser, timeout=10).until(EC.presence_of_element_located((
-    By.CSS_SELECTOR,
-    "#iconmenu_vert\\:panelLocalizarProtocoloEletronico"
-)))
+element = wait_element(browser, By.XPATH, '//*[@id="iconmenu_vert:j_id103"]')
 browser.execute_script("arguments[0].click();", element)
 time.sleep(3)
 
-
-#ano_protocolo = browser.find_element(By.ID, "protocolo_consult_params:ano_protocolo")
-sequencial_protocolo = browser.find_element(By.ID, "protocolo_consult_params:sequencial_protocolo")
-sequencial_protocolo.send_keys("359894")
-
-btnPesquisar = WebDriverWait(browser, timeout=10).until(EC.presence_of_element_located((
-    By.ID,
-    "protocolo_consult_params:botaoPesquisar"
-)))
-
-browser.execute_script("arguments[0].click();", btnPesquisar)
-time.sleep(3)
+find_process(browser)
